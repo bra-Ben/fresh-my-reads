4c4661c198e434c45508908a5c0afb4ec2f28cbe
bra-Ben <74235145+bra-Ben@users.noreply.github.com>
2021-09-08 20:44:10 +0000

    2nd commit

---
+++ src/BookSearch.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import SearchResults from './SearchResults';
+import { Link } from 'react-router-dom';
+import SearchBar from './SearchBar';
+
+class bookSearch extends Component {
+  render() {
+    const { searchBooks, myBooks, onSearch, onResetSearch, onMove } = this.props;
+    
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link to="/">
+            <button className="close-search" onClick={onResetSearch}>
+              Close
+            </button>
+          </Link>
+          <SearchBar onSearch={onSearch} />
+        </div>
+        <SearchResults
+          bookSearch={bookSearch}
+          myBooks={myBooks}
+          onMove={onMove}
+        />
+      </div>
+    );
+  }
+}
+
+export default bookSearch;
